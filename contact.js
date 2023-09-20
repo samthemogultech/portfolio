ef26586426182582ad5b0b80a927dff67994a1b6
Samuel Emeka <143851774+samthemogultech@users.noreply.github.com>
2023-09-20 23:46:24 +0100

    Update contact.js

--- contact.js
+++ contact.js
@@ -21,3 +21,36 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('is-active');
     mobileNav.classList.toggle('is-active');
 })
+
+//Contact Form
+
+window.alert = (message, timeout=null) => {
+    const alert = document.createElement('div');
+    alert.classList.add('message-sent');
+    const button = document.createElement('button');
+    button.classList.add('close-msg-status');
+    button.innerText = "Close"
+    alert.innerHTML = `<i class="fa-regular fa-circle-check"></i>
+    <p>${message}</p>`;
+    alert.appendChild(button)
+    button.addEventListener('click', () => {
+        alert.remove();
+    })
+    document.body.appendChild(alert)
+}
+
+function sendEmail(){
+    senderName = document.getElementById('name').value;
+    senderEmail = document.getElementById('email_id').value;
+    messageBody = document.getElementById('message').value;
+
+    var params = {
+        from_name : senderName,
+        email_id : senderEmail,
+        message : messageBody
+    }
+
+    emailjs.send("service_h1h73za", "template_bbiiz6p", params).then((res) => {
+            alert(`Thanks ${senderName} for reaching out. I'll get back to you.`);
+    })
+}
